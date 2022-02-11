6d90a0b4f4ec7e2e03e314fea40985bbe6098b02
Eze Morales <ezequielmorales5@icloud.com>
2022-02-11 18:23:22 -0300

    Spring Init

---
+++ src/main/java/com/ezequiel/challengebackendma/ChallengeBackendMaApplication.java
@@ -0,0 +1,13 @@
+package com.ezequiel.challengebackendma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeBackendMaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeBackendMaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ezequiel/challengebackendma/ChallengeBackendMaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezequiel.challengebackendma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeBackendMaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
